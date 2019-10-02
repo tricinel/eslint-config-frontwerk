b2e0e36e079eba92f7fcb38483494422028796af
Bogdan Lazar <bogdan@bogdanlazar.com>
2019-10-02 10:22:19 +0200

    Finish variables rules

--- rules/variables.js
+++ rules/variables.js
@@ -16,6 +16,46 @@ module.exports = {
 
     // Disallow specific global variables likely used without intent, such as "event"
     // https://github.com/eslint/eslint/blob/master/docs/rules/no-restricted-globals.md
-    'no-restricted-globals': ['error'].concat(confusingBrowserGlobals)
+    'no-restricted-globals': ['error'].concat(confusingBrowserGlobals),
+
+    // Eliminate shadowed variable declarations
+    // https://github.com/eslint/eslint/blob/master/docs/rules/no-shadow.md
+    'no-shadow': ['error', { builtinGlobals: true }],
+
+    // Don't redefine restricted names like NaN, Infinity or undefined to mean something else
+    // https://github.com/eslint/eslint/blob/master/docs/rules/no-shadow-restricted-names.md
+    'no-shadow-restricted-names': 'error',
+
+    // Disallow the use of undeclared variables, unless mentioned in a /* global */ block
+    // https://github.com/eslint/eslint/blob/master/docs/rules/no-undef.md
+    'no-undef': 'error',
+
+    // Variables that are declared and not initialized automatically gets the value of undefined
+    // https://github.com/eslint/eslint/blob/master/docs/rules/no-undef-init.md
+    'no-undef-init': 'error',
+
+    // Don't overwrite the value of undefined
+    // https://github.com/eslint/eslint/blob/master/docs/rules/no-undefined.md
+    // No need to have this rule on, since we have 'no-shadow-restricted-names' on
+    'no-undefined': 'off',
+
+    // Disallow the use of variables that are declared and not used anywhere in the code
+    // https://github.com/eslint/eslint/blob/master/docs/rules/no-unused-vars.md
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_'
+      }
+    ],
+
+    // Disallow the use of variables before they are defined
+    // https://github.com/eslint/eslint/blob/master/docs/rules/no-use-before-define.md
+    'no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true }
+    ]
   }
 };
