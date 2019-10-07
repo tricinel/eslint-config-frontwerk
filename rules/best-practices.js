711dd5a03baa49d46fd6b2ecc4500f0bdd3720b1
Bogdan Lazar <bogdan@bogdanlazar.com>
2019-10-07 15:31:18 +0200

    Add unused rule no-restricted-properties

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -212,6 +212,61 @@ module.exports = {
     // https://github.com/eslint/eslint/blob/master/docs/rules/no-redeclare.md
     'no-redeclare': 'error',
 
+    // Disallow certain properties on objects
+    // I will trust Airbnb's guide here (https://eslint.org/docs/rules/no-restricted-properties)
+    // https://eslint.org/docs/rules/no-restricted-properties
+    'no-restricted-properties': [
+      'error',
+      {
+        object: 'arguments',
+        property: 'callee',
+        message: 'arguments.callee is deprecated'
+      },
+      {
+        object: 'global',
+        property: 'isFinite',
+        message: 'Please use Number.isFinite instead'
+      },
+      {
+        object: 'self',
+        property: 'isFinite',
+        message: 'Please use Number.isFinite instead'
+      },
+      {
+        object: 'window',
+        property: 'isFinite',
+        message: 'Please use Number.isFinite instead'
+      },
+      {
+        object: 'global',
+        property: 'isNaN',
+        message: 'Please use Number.isNaN instead'
+      },
+      {
+        object: 'self',
+        property: 'isNaN',
+        message: 'Please use Number.isNaN instead'
+      },
+      {
+        object: 'window',
+        property: 'isNaN',
+        message: 'Please use Number.isNaN instead'
+      },
+      {
+        property: '__defineGetter__',
+        message: 'Please use Object.defineProperty instead.'
+      },
+      {
+        property: '__defineSetter__',
+        message: 'Please use Object.defineProperty instead.'
+      },
+      {
+        object: 'Math',
+        property: 'pow',
+        message: 'Use the exponentiation operator (**) instead.'
+      }
+    ],
+
     // Disallow assignments in a return statement, e.g. return foo = bar + 1;
     // https://github.com/eslint/eslint/blob/master/docs/rules/no-return-assign.md
     'no-return-assign': ['error', 'always'],
